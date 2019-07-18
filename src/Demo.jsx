e59f2af6ab30ff1db4677b297a6758f1e50f8c40
Jakub Szewczyk <jakub.szewczykk@gmail.com>
2019-07-18 08:21:10 +0200

    Upgrade dependencies

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -57,19 +57,46 @@ export default class App extends Component {
     })
     setTimeout(() => {
       this.setState({ fakingRequest: false, success: true })
-      rewardPunish === 'reward' ? this.reward.rewardMe() : this.reward.punishMe()
+      rewardPunish === 'reward'
+        ? this.reward.rewardMe()
+        : this.reward.punishMe()
     }, 1500)
   }
 
-  rewardTypeChange = (e) => { console.log(defaults[e.target.value]); this.setState({ type: e.target.value, ...defaults[e.target.value] }) }
-  lifetimeChange = (value) => { this.setState({ lifetime: value }) }
-  angleChange = (value) => { this.setState({ angle: value }) }
-  decayChange = (value) => { if (!isNaN(value)) { this.setState({ decay: value }) } }
-  spreadChange = (value) => { this.setState({ spread: value }) }
-  startVelocityChange = (value) => { this.setState({ startVelocity: value }) }
-  elementCountChange = (value) => { this.setState({ elementCount: value }) }
-  elementSizeChange = (value) => { this.setState({ elementSize: value }) }
-  rewardPunishChange = (e) => { this.setState({ rewardPunish: e.target.value }) }
+  rewardTypeChange = e => {
+    this.setState({ type: e.target.value, ...defaults[e.target.value] })
+  }
+  lifetimeChange = value => {
+    if (isNaN(value)) return
+    this.setState({ lifetime: value })
+  }
+  angleChange = value => {
+    if (isNaN(value)) return
+    this.setState({ angle: value })
+  }
+  decayChange = value => {
+    if (isNaN(value)) return
+    this.setState({ decay: value })
+  }
+  spreadChange = value => {
+    if (isNaN(value)) return
+    this.setState({ spread: value })
+  }
+  startVelocityChange = value => {
+    if (isNaN(value)) return
+    this.setState({ startVelocity: value })
+  }
+  elementCountChange = value => {
+    if (isNaN(value)) return
+    this.setState({ elementCount: value })
+  }
+  elementSizeChange = value => {
+    if (isNaN(value)) return
+    this.setState({ elementSize: value })
+  }
+  rewardPunishChange = e => {
+    this.setState({ rewardPunish: e.target.value })
+  }
 
   render () {
     const {
@@ -91,7 +118,9 @@ export default class App extends Component {
       <div style={containerStyle}>
         <img style={logoStyle} src={logo} />
         <Reward
-          ref={(ref) => { this.reward = ref }}
+          ref={ref => {
+            this.reward = ref
+          }}
           type={type}
           config={{
             lifetime,
@@ -117,7 +146,12 @@ export default class App extends Component {
         <div style={cardStyle}>
           <Row style={rowStyle}>
             <Col style={colStyle} span={24}>
-              <Radio.Group defaultValue='confetti' buttonStyle='solid' onChange={this.rewardTypeChange} disabled={disabled}>
+              <Radio.Group
+                defaultValue='confetti'
+                buttonStyle='solid'
+                onChange={this.rewardTypeChange}
+                disabled={disabled}
+              >
                 <Radio.Button value='confetti'>Confetti 🎉</Radio.Button>
                 <Radio.Button value='emoji'>Emoji 👍</Radio.Button>
                 <Radio.Button value='memphis'>Memphis 🔸</Radio.Button>
@@ -126,19 +160,73 @@ export default class App extends Component {
           </Row>
           <Row style={rowStyle}>
             <Col style={colStyle} span={24}>
-              <Radio.Group defaultValue='reward' buttonStyle='solid' onChange={this.rewardPunishChange}>
+              <Radio.Group
+                defaultValue='reward'
+                buttonStyle='solid'
+                onChange={this.rewardPunishChange}
+              >
                 <Radio.Button value='reward'>Reward</Radio.Button>
                 <Radio.Button value='punish'>Punish</Radio.Button>
               </Radio.Group>
             </Col>
           </Row>
-          <ConfigSlider title='lifetime' inputValue={lifetime} min={0} max={360} onChange={this.lifetimeChange} disabled={disabled} />
-          <ConfigSlider title='angle' inputValue={angle} min={0} max={360} onChange={this.angleChange} disabled={disabled} />
-          <ConfigSlider title='decay' inputValue={decay} min={0} max={1} step={0.01} onChange={this.decayChange} disabled={disabled} />
-          <ConfigSlider title='spread' inputValue={spread} min={0} max={360} onChange={this.spreadChange} disabled={disabled} />
-          <ConfigSlider title='startVelocity' inputValue={startVelocity} min={1} max={100} onChange={this.startVelocityChange} disabled={disabled} />
-          <ConfigSlider title='elementCount' inputValue={elementCount} min={1} max={256} onChange={this.elementCountChange} disabled={disabled} />
-          <ConfigSlider title='elementSize' inputValue={elementSize} min={1} max={50} onChange={this.elementSizeChange} disabled={disabled} />
+          <ConfigSlider
+            title='lifetime'
+            inputValue={lifetime}
+            min={0}
+            max={360}
+            onChange={this.lifetimeChange}
+            disabled={disabled}
+          />
+          <ConfigSlider
+            title='angle'
+            inputValue={angle}
+            min={0}
+            max={360}
+            onChange={this.angleChange}
+            disabled={disabled}
+          />
+          <ConfigSlider
+            title='decay'
+            inputValue={decay}
+            min={0}
+            max={1}
+            step={0.01}
+            onChange={this.decayChange}
+            disabled={disabled}
+          />
+          <ConfigSlider
+            title='spread'
+            inputValue={spread}
+            min={0}
+            max={360}
+            onChange={this.spreadChange}
+            disabled={disabled}
+          />
+          <ConfigSlider
+            title='startVelocity'
+            inputValue={startVelocity}
+            min={1}
+            max={100}
+            onChange={this.startVelocityChange}
+            disabled={disabled}
+          />
+          <ConfigSlider
+            title='elementCount'
+            inputValue={elementCount}
+            min={1}
+            max={256}
+            onChange={this.elementCountChange}
+            disabled={disabled}
+          />
+          <ConfigSlider
+            title='elementSize'
+            inputValue={elementSize}
+            min={1}
+            max={50}
+            onChange={this.elementSizeChange}
+            disabled={disabled}
+          />
         </div>
       </div>
     )
@@ -155,7 +243,8 @@ const containerStyle = {
   padding: 30,
   borderTop: '15px solid #e96443',
   borderBottom: '15px solid #904e95',
-  backgroundImage: 'linear-gradient(#e96443, #904e95), linear-gradient(#e96443, #904e95)',
+  backgroundImage:
+    'linear-gradient(#e96443, #904e95), linear-gradient(#e96443, #904e95)',
   backgroundSize: '15px 100%',
   backgroundPosition: '0 0, 100% 0',
   backgroundRepeat: 'no-repeat',
@@ -163,7 +252,6 @@ const containerStyle = {
 }
 
 const logoStyle = {
-  width: '90%',
   maxWidth: 400,
   paddingBottom: 100
 }
